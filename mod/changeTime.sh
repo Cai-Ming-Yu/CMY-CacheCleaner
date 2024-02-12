c934a77b50a72048113632d792ce028d54f09ce6
彩銘羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2024-02-13 02:02:53 +0800

    Try

--- mod/changeTime.sh
+++ mod/changeTime.sh
@@ -4,5 +4,5 @@ fileDir="$(dirname "$(readlink -f "${0}")")"
 modDir="${fileDir}/source"
 
 for path in $(find "${modDir}"); do
-    touch -c -t 000001010000 "${path}" || touch -c -t 197101010000 "${path}" || touch -c -t 200001010000 "${path}"
+    touch -c -t 000001010000 "${path}" 2>/dev/null || touch -c -t 197101010000 "${path}" 2>/dev/null || touch -c -t 200001010000 "${path}" 2>/dev/null
 done
