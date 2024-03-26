a13a9f4d1c20f43cb94241345e809b584fcd320a
彩銘羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2024-03-27 07:10:33 +0800

    opt

--- mod/source/service.sh
+++ mod/source/service.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
-
 modDir="$('dirname' "$('readlink' '-f' "${0}")")"
 
-[[ ! -d "/data/adb/C-M-Y/CacheCleaner" ]] && 'mkdir' '-p' "/data/adb/C-M-Y/CacheCleaner"
-[[ ! -f "/data/adb/C-M-Y/CacheCleaner/config.yaml" ]] && {
+[ ! -d "/data/adb/C-M-Y/CacheCleaner" ] && 'mkdir' '-p' "/data/adb/C-M-Y/CacheCleaner"
+[ ! -f "/data/adb/C-M-Y/CacheCleaner/config.yaml" ] && {
     'cp' '-rf' "${modDir}/config.yaml" "/data/adb/C-M-Y/CacheCleaner/config.yaml"
 }
 
-while [[ $('getprop' 'sys.boot_completed') != '1' || ! -f '/sdcard/.CacheCleaner' ]]; do
-    'true' >'/sdcard/.CacheCleaner'
+while [ $('getprop' 'sys.boot_completed') != '1' ] ; do
+    'sleep' '1'
+done
+[ -f '/sdcard/.CacheCleaner' ] && 'rm' '-rf' '/sdcard/.CacheCleaner'
+while [ ! -f '/sdcard/.CacheCleaner' ] ; do
+    'true' > '/sdcard/.CacheCleaner'
     'sleep' '1'
 done
 'rm' '-rf' '/sdcard/.CacheCleaner'
 
 'sleep' '60'
 
-[[ "$('pidof' 'CacheCleaner')" != '' ]] && 'killall' '-9' 'CacheCleaner'
+[ "$('pidof' 'CacheCleaner')" != '' ] && 'killall' '-9' 'CacheCleaner'
 
 'chmod' '0777' "${modDir}/CacheCleaner"
-'nohup' "${modDir}/CacheCleaner" "/data/adb/C-M-Y/CacheCleaner/config.yaml" >'/dev/null' 2>&'1' &
+'nohup' "${modDir}/CacheCleaner" "/data/adb/C-M-Y/CacheCleaner/config.yaml" > '/dev/null' 2>&'1' &
 
 'exit' '0'
