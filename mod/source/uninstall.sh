16d73b2cf09f28a231b9ca563975936968abb49e
彩銘羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2024-03-27 07:17:35 +0800

    opt

--- mod/source/uninstall.sh
+++ mod/source/uninstall.sh
@@ -1,4 +1,4 @@
 [ -d "/data/adb/C-M-Y/CacheCleaner" ] && 'rm' '-rf' "/data/adb/C-M-Y/CacheCleaner"
-[[ -d "/data/adb/C-M-Y" && "$('ls' '-A' "/data/adb/C-M-Y")" == '' ]] && 'rm' '-rf' "/data/adb/C-M-Y"
+{ [ -d "/data/adb/C-M-Y" ] && [ "$('ls' '-A' "/data/adb/C-M-Y")" == '' ] ; } && 'rm' '-rf' "/data/adb/C-M-Y"
 
 'exit' '0'
