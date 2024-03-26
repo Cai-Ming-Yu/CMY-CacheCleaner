35a59934704c799f0b324c9229fbd61699168c9d
彩銘羽 <148033104+Cai-Ming-Yu@users.noreply.github.com>
2024-03-27 07:04:33 +0800

    opt

--- mod/source/customize.sh
+++ mod/source/customize.sh
@@ -1,16 +1,10 @@
-#!/bin/sh
-
-SKIPMOUNT='false'
-
-[[ ! "${KSU}" ]] && KSU='false'
-
-[[ "${API}" -lt '21' ]] && {
+[ "${API}" -lt '21' ] && {
     'ui_print' "! API ${API} is too old"
     'ui_print' "- Cache Cleaner is not supported on this Android version"
     'eval' 'abort'
 }
 
-[[ "${BOOTMODE}" && "$('pm' 'path' 'Cai_Ming_Yu.CacheCleaner')" == '' ]] && {
+[ "${BOOTMODE}" && "$('pm' 'path' 'Cai_Ming_Yu.CacheCleaner')" == '' ] && {
     'ui_print' "! Cache Cleaner xposed version is not installed."
     'ui_print' "- Please install it for a better experience."
 }
@@ -20,7 +14,7 @@ verifyList="$('unzip' '-p' "${ZIPFILE}" "files.conf")"
 verifyFile() {
     'local' trueSum="$('echo' '-n' "${verifyList}" | 'grep' " ${1}" | 'awk' '{print $1}')"
     'unzip' '-oj' "${ZIPFILE}" "${1}" '-d' "${MODPATH}/${2}" 1>&'2'
-    [[ "${trueSum}" != "$('md5sum' '-b' "${MODPATH}/${2}/$('basename' "${1}")" | 'cut' -d' ' '-f1')" ]] && {
+    [ "${trueSum}" != "$('md5sum' '-b' "${MODPATH}/${2}/$('basename' "${1}")" | 'cut' -d' ' '-f1')" ] && {
         'ui_print' "! Failed to extract the file: ${1}"
         'ui_print' "- Please go to https://github.com/Cai-Ming-Yu/CMY-CacheCleaner to re-download the module!"
         'eval' 'abort'
@@ -35,12 +29,12 @@ verifyFile() {
 'verifyFile' "bin/${ARCH}/CacheCleaner"
 'verifyFile' 'config.yaml'
 
-[[ ! -d "/data/adb/C-M-Y/CacheCleaner" ]] && 'mkdir' '-p' "/data/adb/C-M-Y/CacheCleaner"
-[[ ! -f "/data/adb/C-M-Y/CacheCleaner/config.yaml" ]] && {
+[ ! -d "/data/adb/C-M-Y/CacheCleaner" ] && 'mkdir' '-p' "/data/adb/C-M-Y/CacheCleaner"
+[ ! -f "/data/adb/C-M-Y/CacheCleaner/config.yaml" ] && {
     'cp' '-rf' "${MODPATH}/config.yaml" "/data/adb/C-M-Y/CacheCleaner/config.yaml"
 }
 
-[[ "$('pgrep' 'CacheCleaner')" != '' ]] && {
+[ "$('pidof' 'CacheCleaner')" != '' ] && {
     'sh' "${MODPATH}/service.sh" &
     'ui_print' "- Cache Cleaner has been restarted"
 } || {
